f138f45c3a2a96cd8ae23786230a2cf8f83cb2bb
Shubhankar Das <shubhankardas@Shubhankars-MacBook-Air.local>
2026-02-21 19:59:33 +0530

    admin endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -1,32 +1,68 @@
 const { Router }=require("express");
 const adminRouter=Router();
 const {adminModel}=require("../db");
+const jwt=require("jsonwebtoken");
+const JWT_USER_PASSWORD="absbba12"
 
-adminRouter.post("/signup", function(req, res){
+adminRouter.post("/signup", async function(req, res){
+
+
+    const { email, password, firstName, lastName }=req.body;
+
+
+
+   await adminModel.create({
+    email:email,
+    password:password,
+    firstName:firstName,
+    lastName:lastName
+   })
     res.json({
-        message:"admin signup"
+        message:"signup succeded"
     })
+   
 })
 
-adminRouter.post("/signin",function(req, res){
-    res.json({
-        message:"admin signin"
+
+
+adminRouter.post("/signin", async function(req, res){
+   const {email, password}=req.body;
+  const admin=await adminModel.findOne({
+    email:email,
+    password:password
+  });
+
+  if(admin){
+    const token = jwt.sign({
+        id: admin._id
+    },JWT_USER_PASSWORD);
+
+    //
+
+   res.json({
+    token:token
+   })
+
+  }else{
+      res.status(404).json({
+        message:"wrong credential"
     })
+  }
 })
 
 
-adminRouter.post("/course",function(req, res){
+adminRouter.post("/",function(req, res){
     res.json({
         message:"admin course endpoint"
     })
 })
 
-adminRouter.put("/course", function(req, res){
+adminRouter.put("/", function(req, res){
     res.json({
         message:"put course endpoint"
     })
 })
-adminRouter.get("/course/bulk", function(req, res){
+adminRouter.get("/bulk", function(req, res){
     res.json({
         message:"course get endpoint"
     })
--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,48 @@
 const { Router } =require("express") ;
-
 const userRouter= Router();
+const { userModel }=require("../db");
+const jwt=require("jsonwebtoken");
+const JWT_USER_PASSWORD="asasa12"
+
+userRouter.post("/signup", async function(req, res){
+   const { email, password, firstName, lastName }=req.body;
 
 
-userRouter.post("/signup", function(req, res){
-  
+
+   await userModel.create({
+    email:email,
+    password:password,
+    firstName:firstName,
+    lastName:lastName
+   })
     res.json({
-        message:"signup"
+        message:"signup succeded"
     })
 
 })
-userRouter.post("/sigin", function(req, res){
-  
-    res.json({
-        message:"signin"
-    })
+userRouter.post("/signin", async function(req, res){
+  const {email, password}=req.body;
+  const user=await userModel.findOne({
+    email:email,
+    password:password
+  });
 
+  if(user){
+    const token = jwt.sign({
+        id: user._id
+    },JWT_USER_PASSWORD);
+
+    //
+
+   res.json({
+    token:token
+   })
+
+  }else{
+      res.status(404).json({
+        message:"wrong credential"
+    })
+  }
 })
 userRouter.get("/purchases", function(req, res){
   
